24a3e91bb9e162151552791696287884d13059fb
DmitryDam <himdecor@ukr.net>
2018-02-19 22:20:15 +0200

    first commit

--- 8.js
+++ 8.js
@@ -1,39 +1,62 @@
-const playSound = note => {
-  const audio = document.querySelector(`audio[data-note=${note}]`);
-  audio.currentTime = 0;
-  audio.play();
-};
-       // Метод Array.from() создаёт новый экземпляр Array из массивоподобного или итерируемого объекта.
-const buttons = Array.from(document.querySelectorAll("button"));
-const keys = "qwertyuiop[]asdfghjkl;'zxcvbnm,./".split("");
-const sound = document.querySelector("#slideThree");
-console.log (sound);
+const firstname = document.getElementById("first_name");
+const lastname = document.getElementById("last_name");
+const tel = document.getElementById("tel");
+const submitBtn = document.getElementById("submit-btn");
+const resultsList = document.querySelector(".results");
 
-let callback = (event) => {
-	for (let i=0; i<keys.length; i++) {
-		if (event.key == keys[i]) {
+const regFirstName = /[A-ZА-ЯІ][a-zа-яії]{2,30}$/;
+const regLastName = /[A-ZА-ЯІ][a-zа-яії]{2,30}$/;
+const regPhone = /[\+]\d{2}[\(]\d{3}[\)]\d{7}$/;
 
-			if (event.key == buttons[i].innerHTML) {
+let form = {};
+let checkBool = {};
+let results;
 
-				buttons[i].classList.add("keyboard__btn--active");
-				setTimeout(() => {
-			      buttons[i].classList.remove("keyboard__btn--active");
-			    }, 500);
-			    // Доступ к атрибутам осуществляется при помощи стандартных методов:
-       //          elem.hasAttribute(name) – проверяет наличие атрибута
-       //          elem.getAttribute(name) – получает значение атрибута
-       //          elem.setAttribute(name, value) – устанавливает атрибут
-       //          elem.removeAttribute(name) – удаляет атрибут
+function validate(evt) {
+// Задаем значения по умолчанию
+	let checkBool = {
+		firstname : false,
+		lastname : false,
+		tel : false,
+	};
+// создается объект формы
+	form.firstname = firstname.value;
+	form.lastname = lastname.value;
+	form.tel = tel.value;
 
-			    if (sound.checked) {
-			    	// debugger
-			    	playSound(buttons[i].getAttribute('data-note'));
-			    	console.log (event);
-			    	console.log (playSound);
-			    }
-			}
-		}
+	// Для поиска точного соответствия объект RegExp 
+	// имеет встроенный метод test, 
+	// возвращающий true или false.
+	
+	if (regFirstName.test(firstname.value)) {
+		checkBool.firstname = true;	
 	}
+
+	if (regLastName.test(lastname.value)) {
+		checkBool.lastname = true;	
+	}
+
+	if (regPhone.test(tel.value)) {
+		checkBool.tel = true;	
+	}
+
+	results = checkBool;
+	console.log(results)
+
+  function showResults(results) {
+	  let out = '';
+	  console.log(results);
+
+	  for (let key in results) {
+	    	if (results[key] == true) {
+			out += `<li class="success">SUCCESS: ${key} passed validation</li>`;
+		    }
+		    else {
+			out += `<li class="error">ERROR: ${key} failed validation</li>`;
+		    }		
+	    };
+	    resultsList.innerHTML = out;
+    }
+  showResults(results) 
 }
-console.log (event);
-window.addEventListener("keydown", callback);
+submitBtn.addEventListener("click", validate);
